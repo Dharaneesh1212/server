5cb87dd41a7d66fb2cb1d792fe949832b064cd89
dharaneesh1212 <dharun12122002@gmail.com>
2024-03-04 14:10:13 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -27,25 +27,31 @@ router.post("/signup", async (req, res) => {
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (!user) {
-    return res.json({ message: "user is not registered" });
+    return res.json({ message: "User is not registered" });
   }
 
   const validPassword = await bcrypt.compare(password, user.password);
+
   if (!validPassword) {
-    return res.json({ message: "password is incorrect" });
+    return res.json({ message: "Password is incorrect" });
   }
+
   const token = jwt.sign({ username: user.username }, process.env.KEY, {
     expiresIn: "1h",
   });
+
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
+    secure: true, // Requires HTTPS
     sameSite: "none",
-    maxAge: 360000,
+    maxAge: 360000, // Adjust based on your session timeout
   });
-  return res.json({ status: true, message: "login successfully" });
+
+  return res.json({ status: true, message: "Login successful" });
 });
+
 
 router.post("/forgot-password", async (req, res) => {
   const { email } = req.body;
